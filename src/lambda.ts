573d0bcba38df49e43942858223e01f080dfdc06
Pat Lillis <lillispm@gmail.com>
2025-03-12 22:10:32 -0600

    WIP

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,14 +1,10 @@
 import { RDSDataService } from "aws-sdk";
+import { ExecuteStatementRequest } from "aws-sdk/clients/rdsdataservice";
 
 const rdsData = new RDSDataService();
 
 export const handler = async () => {
-    const params: {
-        resourceArn: string;
-        secretArn: string;
-        sql: string;
-        database: string;
-    } = {
+    const params: ExecuteStatementRequest = {
         resourceArn: process.env.DB_ARN || "", // Add the ARN of the database cluster
         secretArn: process.env.DB_SECRET_ARN || "", // Add the ARN of the secret
         sql: "SELECT * FROM your_table_name LIMIT 1;", // Replace with your actual table name
