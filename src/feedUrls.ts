3044ab940e65dc57379e3dc8b1b5f499c343b9aa
Pat Lillis <lillispm@gmail.com>
2025-04-15 22:05:43 -0600

    Add the Orbital Index

--- src/feedUrls.ts
+++ src/feedUrls.ts
@@ -2,4 +2,5 @@ export const feedUrls = [
   "https://www.robinsloan.com/lab.xml",
   "https://feeds.hanselman.com/ScottHanselman",
   "https://medium.com/feed/@jatinchowdhury18",
+  "https://orbitalindex.com/feed.xml",
 ];
